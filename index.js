4cab88a0ff916b67598cac795d5c4763d6b9ffb5
shahin2525 <shahin451999@gmail.com>
2024-11-08 23:45:33 -0800

    add cooci crendential

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import cookie from "cookie-parser";
 
 import cors from "cors";
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -33,8 +39,16 @@ async function run() {
 
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, "secret", { expiresIn: "1h" });
-      res.send(token);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "none",
+          secure: false,
+        })
+        .send({ success: true });
     });
     // service related api
     app.get("/services", async (req, res) => {
